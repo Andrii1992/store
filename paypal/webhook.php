945e829a00d0bbffcbc4f390de63ca609e3128a9
Andrii1992 <super.80966898912@yandex.ru>
2021-10-22 22:43:02 +0200

    Correct webhook

--- paypal/webhook.php
+++ paypal/webhook.php
@@ -12,6 +12,9 @@ use \PayPal\Api\WebhookEvent;
 
 function get_header(string $name) : string {
   $name = str_replace("-","_", $name);
+  if(is_null($name)){
+      return "";
+  }
   return $_SERVER["HTTP_$name"];
 }
 
@@ -29,12 +32,12 @@ $apiContext->setConfig([
 $requestBody = file_get_contents('php://input'); // info type event 
 
 $signatureVerification = new VerifyWebhookSignature();
-$signatureVerification->setAuthAlgo(get_header("PAYPAL-AUTH-ALGO"));
-$signatureVerification->setTransmissionId(get_header("PAYPAL-TRANSMISSION-ID"));
-$signatureVerification->setCertUrl(get_header("PAYPAL-CERT-URL"));
+$signatureVerification->setAuthAlgo(get_header('PAYPAL-AUTH-ALGO'));
+$signatureVerification->setTransmissionId(get_header('PAYPAL-TRANSMISSION-ID'));
+$signatureVerification->setCertUrl(get_header('PAYPAL-CERT-URL'));
 $signatureVerification->setWebhookId(PAYPAL_WEBHOOK_ID);
-$signatureVerification->setTransmissionSig(get_header("PAYPAL-TRANSMISSION-SIG"));
-$signatureVerification->setTransmissionTime(get_header("PAYPAL-TRANSMISSION-TIME"));
+$signatureVerification->setTransmissionSig(get_header('PAYPAL-TRANSMISSION-SIG'));
+$signatureVerification->setTransmissionTime(get_header('PAYPAL-TRANSMISSION-TIME'));
 
 $signatureVerification->setRequestBody($requestBody);
 $request = clone $signatureVerification;
